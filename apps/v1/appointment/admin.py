846df517ef54aa4bd83e02c7d775acde1ba75221
dalikuziev <dalikuziev@gmail.com>
2025-05-21 13:31:54 +0500

    2025.05.21/13:31

--- apps/v1/appointment/admin.py
+++ apps/v1/appointment/admin.py
@@ -9,6 +9,10 @@ class AppointmentResource(resources.ModelResource):
     class Meta:
         model = Appointment
 
+class RateResource(resources.ModelResource):
+    class Meta:
+        model = Rate
+
 class ReasonResource(resources.ModelResource):
     class Meta:
         model = Reason
@@ -21,32 +25,37 @@ class ReviewLikeResource(resources.ModelResource):
     class Meta:
         model = ReviewLike
 
-class RateResource(resources.ModelResource):
-    class Meta:
-        model = Rate
-
-
 @admin.register(Appointment)
 class AppointmentAdmin(ImportExportModelAdmin, BaseAdmin):
     resource_classes = [AppointmentResource]
-    list_display = tuple(f.name for f in Appointment._meta.fields if f.name)
-
-@admin.register(Reason)
-class ReasonAdmin(ImportExportModelAdmin, BaseAdmin):
-    resource_classes = [ReasonResource]
-    list_display = tuple(f.name for f in Reason._meta.fields if f.name)
-
-@admin.register(Review)
-class ReviewAdmin(ImportExportModelAdmin, BaseAdmin):
-    resource_classes = [ReviewResource]
-    list_display = tuple(f.name for f in Review._meta.fields if f.name)
-
-@admin.register(ReviewLike)
-class ReviewLikeAdmin(ImportExportModelAdmin, BaseAdmin):
-    resource_classes = [ReviewLikeResource]
-    list_display = tuple(f.name for f in ReviewLike._meta.fields if f.name)
+    list_display = tuple(f.name for f in Appointment._meta.fields if f.name not in (
+        'id',
+    ))
 
 @admin.register(Rate)
 class RateAdmin(ImportExportModelAdmin, BaseAdmin):
     resource_classes = [RateResource]
-    list_display = tuple(f.name for f in Rate._meta.fields if f.name)
+    list_display = tuple(f.name for f in Rate._meta.fields if f.name not in (
+        'id',
+    ))
+
+@admin.register(Reason)
+class ReasonAdmin(ImportExportModelAdmin, BaseAdmin):
+    resource_classes = [ReasonResource]
+    list_display = tuple(f.name for f in Reason._meta.fields if f.name not in (
+        'id',
+    ))
+
+@admin.register(Review)
+class ReviewAdmin(ImportExportModelAdmin, BaseAdmin):
+    resource_classes = [ReviewResource]
+    list_display = tuple(f.name for f in Review._meta.fields if f.name not in (
+        'id',
+    ))
+
+@admin.register(ReviewLike)
+class ReviewLikeAdmin(ImportExportModelAdmin, BaseAdmin):
+    resource_classes = [ReviewLikeResource]
+    list_display = tuple(f.name for f in ReviewLike._meta.fields if f.name not in (
+        'id',
+    ))
